6b3e7db8485d70ea1c245a0c2d9ff3e60916b873
gaohuan <1072826544@qq.com>
2023-05-17 19:47:51 +0800

    second commit

---
+++ get_msxe3711_data.cpp
@@ -0,0 +1,6 @@
+#include "get_msxe3711_data.h"
+
+get_msxe3711_data::get_msxe3711_data()
+{
+
+}
---
+++ get_msxe3711_data.h
@@ -0,0 +1,11 @@
+#ifndef GET_MSXE3711_DATA_H
+#define GET_MSXE3711_DATA_H
+
+
+class get_msxe3711_data
+{
+public:
+    get_msxe3711_data();
+};
+
+#endif // GET_MSXE3711_DATA_H
